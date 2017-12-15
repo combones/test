45fc42929d8193e143e70a257e4235703acb8666
combones <31252221+combones@users.noreply.github.com>
2017-12-15 23:43:42 +0700

    Update character.js

--- models/character.js
+++ models/character.js
@@ -3,9 +3,7 @@ var mongoose = require('mongoose');
 var characterSchema = new mongoose.Schema({
   characterId: { type: String, unique: true, index: true },
   name: String,
-  race: String,
-  gender: String,
-  bloodline: String,
+  type: String,
   wins: { type: Number, default: 0 },
   losses: { type: Number, default: 0 },
   reports: { type: Number, default: 0 },
