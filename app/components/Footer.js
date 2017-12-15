9bd77339bc163a67f27cfce79f29ace7dd58833d
combones <31252221+combones@users.noreply.github.com>
2017-12-15 23:14:20 +0700

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -28,7 +28,7 @@ class Footer extends React.Component {
       return (
         <li key={character.characterId}>
           <Link to={'/characters/' + character.characterId}>
-            <img className='thumb-md' src={'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg'} />
+            <img className='thumb-md' src={'https://www.owfire.com/images/heroes/potrait/'+character.name+'.png'} />
           </Link>
         </li>
       );
@@ -38,12 +38,6 @@ class Footer extends React.Component {
       <footer>
         <div className='container'>
           <div className='row'>
-            <div className='col-sm-5'>
-              <h3 className='lead'><strong>Information</strong> and <strong>Copyright</strong></h3>
-              <p>Powered by <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>React</strong> with Flux architecture and server-side rendering.</p>
-              <p>You may view the <a href='https://github.com/sahat/newedenfaces-react'>Source Code</a> behind this project on GitHub.</p>
-              <p>© 2015 Sahat Yalkabov.</p>
-            </div>
             <div className='col-sm-7 hidden-xs'>
               <h3 className='lead'><strong>Leaderboard</strong> Top 5 Characters</h3>
               <ul className='list-inline'>
