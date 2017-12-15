0d040fa2c303b2a56012494b7224dd952504f6b1
combones <31252221+combones@users.noreply.github.com>
2017-12-15 23:50:51 +0700

    Update StatsStore.js

--- app/stores/StatsStore.js
+++ app/stores/StatsStore.js
@@ -5,15 +5,12 @@ import StatsActions from '../actions/StatsActions';
 class StatsStore {
   constructor() {
     this.bindActions(StatsActions);
-    this.leadingRace = { race: 'Unknown', count: 0 };
-    this.leadingBloodline = { bloodline: 'Unknown', count: 0 };
-    this.amarrCount = 0;
-    this.caldariCount = 0;
-    this.gallenteCount = 0;
-    this.minmatarCount = 0;
+    this.leadingType = { Type: 'Unknown', count: 0 };
     this.totalVotes = 0;
-    this.femaleCount = 0;
-    this.maleCount = 0;
+    this.OffenseCount = 0;
+    this.DefenseCount = 0;
+    this.TankCount = 0;
+    this.SupportCount = 0;
     this.totalCount = 0;
   }
 
