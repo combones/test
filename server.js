52bc53a00c046b44135e26bbeb7e133f070cc560
combones <31252221+combones@users.noreply.github.com>
2017-12-15 23:29:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'ow.png')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
